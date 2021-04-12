5ec62c246070ef080d3209b7481a3fea0a6c1560
Mariano Salguero <salguero.ms@gmail.com>
2021-04-12 00:45:47 -0300

    fixs auth completed++

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 document.getElementById("but-auth").addEventListener('click', auth);
 
 function auth() {
-    window.location.href = "https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=879324550414154&redirect_uri=https://marianslg.github.io/mlsql/";
+    window.location.href = "https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=879324550414154&redirect_uri=https://marianslg.github.io/mlsql";
 }
