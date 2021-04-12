806f3811ad6012a222ab02ff03ed3c99c6113928
Mariano Salguero <salguero.ms@gmail.com>
2021-04-12 00:18:14 -0300

    fixs auth completed

--- auth.js
+++ auth.js
@@ -1,27 +1,5 @@
 document.getElementById("but-auth").addEventListener('click', auth);
-document.getElementById("but-borrar-acc").addEventListener('click', borraAcc);
-document.getElementById("but-borrar-ref").addEventListener('click', borrarRef);
-document.getElementById("but-mod-acc").addEventListener('click', modAcc);
-document.getElementById("but-mod-ref").addEventListener('click', modRef);
 
 function auth() {
     window.location.href = "https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=879324550414154&redirect_uri=https://marianslg.github.io/mlsql/";
 }
-
-
-function borrarRef() {
-    localStorage.removeItem("ref_token");
-}
-
-function borraAcc() {
-    localStorage.removeItem("acc_token");
-}
-
-function modRef() {
-    localStorage.setItem("ref_token", "json.refresh_token");
-}
-
-function modAcc() {
-    localStorage.setItem("acc_token", "json.access_token");
-}
-
--- index.js
+++ index.js
@@ -29,23 +29,15 @@ var urlParams = new URLSearchParams(window.location.search);
 var ref_token = localStorage.getItem('ref_token');
 var acc_token = localStorage.getItem('acc_token');
 
-console.log(ref_token)
-console.log(acc_token)
-
-var loc = "";
-
 if (acc_token != null) {
     if (await test_token()) {
-        // window.location.href = "search.html";
-        loc = "search.html";
+        window.location.href = "search.html";
     } else if (ref_token != null) {
         let result_refresh = await refresh_token();
 
         if (result_refresh.status == 200) {
             localStorage.setItem("acc_token", result_refresh.access_token);
-            loc = "search.html";
-
-            //   window.location.href = "search.html";
+            window.location.href = "search.html";
         }
     }
 }
@@ -58,20 +50,10 @@ if (urlParams.has('code')) {
     if (json.refresh_token != undefined && json.access_token != undefined) {
         localStorage.setItem("ref_token", json.refresh_token);
         localStorage.setItem("acc_token", json.access_token);
-
-        loc = "search.html";
-        //   window.location.href = "search.html";
+        window.location.href = "search.html";
     } else {
         alert("Error en autenticación: \n" + json.message);
     }
 }
 
-document.getElementById("but-ir").addEventListener('click', ir);
-
-function ir() {
-    if (loc != "") {
-        window.location.href = loc;
-    } else {
-        window.location.href = "auth.html" + (urlParams.get('code') != null ? "?code=" + urlParams.get('code') : "");
-    }
-}
+window.location.href = "auth.html";
