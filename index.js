2f5c66325dadbb166d11d591e122ddb729533575
Mariano Salguero <salguero.ms@gmail.com>
2021-04-12 00:41:25 -0300

    fixs auth completed+

--- index.js
+++ index.js
@@ -28,19 +28,20 @@ else
 var urlParams = new URLSearchParams(window.location.search);
 var ref_token = localStorage.getItem('ref_token');
 var acc_token = localStorage.getItem('acc_token');
+var href = "";
 
 if (acc_token != null) {
     if (await test_token()) {
-        window.location.href = "search.html";
+        href = "search.html";
     } else if (ref_token != null) {
         let result_refresh = await refresh_token();
 
         if (result_refresh.status == 200) {
             localStorage.setItem("acc_token", result_refresh.access_token);
-            window.location.href = "search.html";
+            href = "search.html";
         }
     }
-}
+}  
 
 if (urlParams.has('code')) {
     let code = urlParams.get('code')
@@ -50,10 +51,10 @@ if (urlParams.has('code')) {
     if (json.refresh_token != undefined && json.access_token != undefined) {
         localStorage.setItem("ref_token", json.refresh_token);
         localStorage.setItem("acc_token", json.access_token);
-        window.location.href = "search.html";
+        href = "search.html";
     } else {
         alert("Error en autenticación: \n" + json.message);
     }
 }
 
-window.location.href = "auth.html";
+window.location.href = (href != "" ? href : "auth.html");
