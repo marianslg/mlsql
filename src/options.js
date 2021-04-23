07eff8b0b258cc7bfaf0fac3e1dfe9ed83bc5068
Mariano Salguero <salguero.ms@gmail.com>
2021-04-23 00:25:52 -0300

    fix query

--- src/options.js
+++ src/options.js
@@ -2,7 +2,7 @@ export const options =
     [
         {
             "name": "Ordenar por más vendidos",
-            "query": "SELECT * FROM Items ORDER BY sold_quantity"
+            "query": "SELECT * FROM Items ORDER BY sold_quantity DESC"
         },
         {
             "name": "Del vendedor con más transacciones, obtener el item con menor precio",
